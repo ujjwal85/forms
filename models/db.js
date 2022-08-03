c253258d320a37e819e7fe094433d0afddc3b541
Ujjwal Singh <ujjwal.singh@softobiz.com>
2022-08-03 10:39:54 +0530

    first commit

---
+++ models/db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const mongodb=require('mongodb')
+
+mongoose.connect('mongodb://localhost:27017/EmployeeDB', { useNewUrlParser: true }, (err) => {
+    if (!err) { console.log('MongoDB Connection Succeeded.') }
+    else { console.log('Error in DB connection : ' + err) }
+});
+
+require('./employee.model');
